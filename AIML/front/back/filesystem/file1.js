71e196a007bc96ef03ca1cd214574554b178a36a
Soumya <sharansoumya4@gmail.com>
2025-02-07 09:56:32 +0530

    new update

--- AIML/front/back/filesystem/file1.js
+++ AIML/front/back/filesystem/file1.js
@@ -1,3 +1,16 @@
+//const fs = require('fs');
+//const data = fs.readFileSync("./data.txt", "utf-8");
+//console.log(data);
+//fs.writeFileSync("data3.txt", "this is my first work" , "utf-8");
+//fs.appendFileSync("./data1.txt", "work from another file", "utf-8");
+//fs.renameSync("./data3.txt", "./data5.txt" );
+//fs.unlinkSync("./data5.txt")
+
 const fs = require('fs');
 const data = fs.readFileSync("./data.txt", "utf-8");
 console.log(data);
+if(data.match("H")){
+    console.log("H is found");
+    const newdata = data.replace("H", "ABES");
+    fs.writeFileSync("./data.txt", newdata, "utf-8");
+}
